68deaa44f7570dba0c91f16b5267f20465e0a22e
Noodle98 <46034007+Noodle98@users.noreply.github.com>
2025-05-16 01:36:19 +0200

    added majority of new items to bazaarItems, and changed bazaar price checker prices to display item name, as item ID instead of user input

--- main.js
+++ main.js
@@ -40,8 +40,8 @@ function mainData(Data, itemName, rawUserInput)
     let buyOrderPrice = Data['products'][itemName]['sell_summary'][0]['pricePerUnit'];
     let sellOfferPrice = Data['products'][itemName]['buy_summary'][0]['pricePerUnit'];
 
-    document.getElementById('buy_order_price').innerHTML = `Current buy order (instant sell) price for ${rawUserInput} is: ${buyOrderPrice.toLocaleString("en-US")} coins.`;
-    document.getElementById('sell_offer_price').innerHTML = `Current sell offer (instant buy) price for ${rawUserInput} is: ${sellOfferPrice.toLocaleString("en-US")} coins.`;
+    document.getElementById('buy_order_price').innerHTML = `Current buy order (instant sell) price for ${itemName} is: ${buyOrderPrice.toLocaleString("en-US")} coins.`;
+    document.getElementById('sell_offer_price').innerHTML = `Current sell offer (instant buy) price for ${itemName} is: ${sellOfferPrice.toLocaleString("en-US")} coins.`;
   }
 // speak() function which is called by the HTML code,
 // And parses user input, and then passes it along to
